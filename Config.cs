c68c33cd9115c90792bee95ab2f50601f2845991
morgana <89588301+morgana-x@users.noreply.github.com>
2023-09-15 18:39:31 +1000

    Zombie count, optimise, hide border by default

--- Config.cs
+++ Config.cs
@@ -16,16 +16,21 @@ namespace SCPTeamStatsv2
         [Description("Text size (PERCENTAGE NOT PIXEL)")]
         public int textSize { get; set; } = 65;
 
-        [Description("Should NPCs be hidden on list")]
-        public bool hideNPCs { get; set; } = true;
-
         [Description("Should display be shown on bottom of the screen")]
         public bool bottomDisplay { get; set; } = false;
+
+        [Description("Should the border be displayed")]
+        public bool showBorder { get; set; } = false;
 
         [Description("Border for SCP stats (1 character only at this time) WARNING USING '{}' DOES NOT WORK AND WILL BUG THE SYSTEM!!!!")]
         public string borderCharacters { get; set; } = "[]";
 
         [Description("Border color")]
         public Color borderColor { get; set; } = Color.white;
+
+        [Description("Should NPCs be hidden on list")]
+        public bool hideNPCs { get; set; } = true;
+        [Description("Show the amount of zombies to all scps")]
+        public bool showZombies { get; set; } = true;
     }
 }
--- EventHandlers.cs
+++ EventHandlers.cs
@@ -67,55 +67,73 @@ namespace SCPTeamStatsv2
         {
             Timing.KillCoroutines(displayHandle);
         }
+        public int getZombieCount()
+        {
+            return Player.List.Where(x => x.Role.Type == RoleTypeId.Scp0492).Count();
+        }
+        public string borderText(string text)
+        {
+            if (!Plugin.Instance.Config.showBorder)
+                return text + " ";
+            return "<color=" + Plugin.Instance.Config.borderColor.ToHex() + "> " +
+                    Plugin.Instance.Config.borderCharacters[0] + "</color>" + text +
+                    "<color=" +
+                    Plugin.Instance.Config.borderColor.ToHex() +
+                    ">" +
+                    Plugin.Instance.Config.borderCharacters[1] +
+                    "</color> "; ;
+        }
+        public string formatPlayerInfo(Player pl)
+        {
+            string cool = "<color=red>" + "SCP-" + pl.Role.Type.ToString().Replace("Scp", string.Empty) + "</color> ";
+
+            if (pl.Role.Type != RoleTypeId.Scp079)
+            {
+                cool = cool + "<color=green>" + Math.Ceiling(pl.Health).ToString() + "</color>";
+                if (pl.HumeShield > 0)
+                    cool = cool + " <color=#5a97fa>" + Math.Ceiling(pl.HumeShield).ToString() + "</color>";
+                if (pl.ArtificialHealth > 0)
+                    cool = cool + " <color=yellow> " + Math.Ceiling(pl.ArtificialHealth).ToString() + "</color>";
+            }
+            else
+            {
+                // todo: Optimise whatever the heck this is
+                cool = cool + "<color=#1081e3>LVL </color><color=#5a97fa>" + pl.Role.As<Scp079Role>().Level.ToString() + "</color>";
+                List<int> gens = getGenerators();
+                int engagedGenerators = gens[0];
+                int activeGenerators = gens[1];
+                if (engagedGenerators > 0 || activeGenerators > 0)
+                {
+                    string col = "#5a97fa";
+                    if (activeGenerators > 0)
+                        col = "#f54c4c";
+                    cool = cool + "<color=#1081e3> GEN </color><color=" + col + ">" + (engagedGenerators + activeGenerators).ToString() + "</color>";
+                }
+            }
+            return borderText(cool);
+        }
         public string formatDisplayExperiment(List<Player> scps)
         {
             string display = string.Empty;
 
             if (Plugin.Instance.Config.bottomDisplay)
-            {
                 display = display + new string('\n', 18);
-            }
 
             display = display + "<size=" + Plugin.Instance.Config.textSize.ToString() + "%>";
 
 
 
             foreach (Player pl in scps)
+                display = display + formatPlayerInfo(pl);
+        
+            if (Plugin.Instance.Config.showZombies)
             {
-                string cool = "<color=red>" + "SCP-" + pl.Role.Type.ToString().Replace("Scp", string.Empty) + "</color> ";
-
-                if (pl.Role.Type != RoleTypeId.Scp079)
-                {
-                    cool = cool + "<color=green>" + Math.Ceiling(pl.Health).ToString() + "</color>";
-                    if (pl.HumeShield >0)
-                        cool = cool + " <color=#5a97fa>" + Math.Ceiling(pl.HumeShield).ToString() + "</color>";
-                    if (pl.ArtificialHealth > 0)
-                        cool = cool + " <color=yellow> " + Math.Ceiling(pl.ArtificialHealth).ToString() + "</color>";
-                }
-                else
-                {
-                    // todo: Optimise whatever the heck this is
-                    cool = cool + "<color=#1081e3>LVL </color><color=#5a97fa>" + pl.Role.As<Scp079Role>().Level.ToString() + "</color>";
-                    List<int> gens = getGenerators();
-                    int engagedGenerators = gens[0];
-                    int activeGenerators = gens[1];
-                    if (engagedGenerators > 0 || activeGenerators > 0)
-                    {
-                        string col = "#5a97fa";
-                        if (activeGenerators > 0)
-                            col = "#f54c4c";
-                        cool = cool + "<color=#1081e3> GEN </color><color=" + col + ">" + (engagedGenerators + activeGenerators).ToString() + "</color>";
-                    }
-                }
-                // todo: Optimise whatever the heck this is
-                display = display + "<color=" + Plugin.Instance.Config.borderColor.ToHex() + ">" + Plugin.Instance.Config.borderCharacters[0] + "</color>" + cool + "<color=" + Plugin.Instance.Config.borderColor.ToHex() + ">" + Plugin.Instance.Config.borderCharacters[1] + "</color> ";
+                int zombies = getZombieCount();
+                if (zombies > 0)
+                    display = display + borderText("<color=red>SCP-049-2</color> <color=#5a97fa>" + zombies.ToString() + "</color>");
             }
-
-
             if (!Plugin.Instance.Config.bottomDisplay)
-            {
                 display = display + new string('\n', 51);
-            }
             return display;
         }
         public IEnumerator<float> refreshDisplay()
@@ -123,24 +141,17 @@ namespace SCPTeamStatsv2
             while (true)
             {
                 List<Player> scps = getSCPs();
-                if (scps.Count <= 0)
-                {
-                    yield return Timing.WaitForSeconds(Plugin.Instance.Config.DisplayRefreshRate);
-                }
-                string displayText = formatDisplayExperiment(scps);
-                foreach (Player pl in Player.List)
-                {
-                    if (!pl.IsScp)
-                    {
-                        continue;
-                    }
-                    if (pl.IsNPC)
-                    {
-                        continue;
-                    }
 
+                if (scps.Count <= 0)
+                    yield return Timing.WaitForSeconds(Plugin.Instance.Config.DisplayRefreshRate);
+
+
+                string displayText = formatDisplayExperiment(scps);
+
+                foreach (Player pl in Player.List.Where(x => x.IsScp && !x.IsNPC))
                     pl.ShowHint(displayText, duration:(Plugin.Instance.Config.DisplayRefreshRate + 1f));
-                }
+
+
                 yield return Timing.WaitForSeconds(Plugin.Instance.Config.DisplayRefreshRate);
             }
         }
